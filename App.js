f3f0494f1dbf13d23d98bf72e1fe8ee2dec59cf3
Gugonunes <gugonunes@hotmail.com>
2023-01-27 18:37:55 -0300

    adicionado ajustes e estilo do aplictivo

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Title from './src/components/title/';
-import Main from './src/components/Main';
-
+import Form from './src/components/Form';
 export default function App() {
   return (
     <View style={styles.container}>
       <Title />
-      <Main />
+      <Form />
     </View>
   );
 }
@@ -15,15 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2A2A2A',
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'auto',
-    paddingTop: 50
-  },
-  title: {
-    color: '#FFF',
-    fontSize: 24,
-    marginBottom: 24
+    backgroundColor: '#e0e5e5',
+    paddingTop: 80
   }
 });
